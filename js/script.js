73440c2701cda90fa7b21e0bc1d1af2bffd1789f
vatspranjal <77121332+vatspranjal@users.noreply.github.com>
2021-07-17 14:34:37 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-let searchBtn = document.querySelector('#search-btn');
-let searchBar = document.querySelector('.search-bar-container');
-let formBtn = document.querySelector('#login-btn');
-let loginForm = document.querySelector('.login-form-container');
-let formClose = document.querySelector('#form-close');
-let menu = document.querySelector('#menu-bar');
-let navbar = document. querySelector('.navbar');
 let videoBtn = document. querySelectorAll('.vid-btn');
 
 var swiper = new Swiper(".review-slider", {
